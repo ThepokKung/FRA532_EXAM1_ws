01bf11d907d5159295b2b40d9fabfb407968199b
ThepokKung <pok23250@gmail.com>
2025-03-07 13:54:27 +0700

    Add .gitignore and remove 'build install log'

--- build/robot_nav2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kraiwich/FRA532_EXAM1_ws/src/robot_nav2
-# Build directory: /home/kraiwich/FRA532_EXAM1_ws/build/robot_nav2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/robot_nav2/ament_cmake_python/robot_nav2/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='robot_nav2',
-    version='0.0.0',
-    packages=find_packages(
-        include=('robot_nav2', 'robot_nav2.*')),
-)
